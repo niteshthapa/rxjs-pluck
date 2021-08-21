087883fda9bdfc2a9581ac78aa14ec80a3789999
Nitesh <nitesh_thapa@ymail.com>
2021-08-21 11:42:43 +0530

    rxjs pluck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { from, interval ,Subscription} from 'rxjs';
+import {map, pluck} from 'rxjs/operators'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pluckrxjs';
+export class AppComponent implements OnInit {
+  arr=[
+    {name:"Nitesh",age:33}, {name:"Sadne",age:23}
+  ]
+  ngOnInit() {
+ 
+ from(this.arr).pipe(
+   pluck('name')
+ ).subscribe(result=>  this.addStream(result,'container2'))
+  }
+  addStream(value,id){
+    let el = document.createElement("li");
+    el.classList.add("list-group-item");
+    el.innerText = value;
+    document.getElementById(id).appendChild(el)
+}
 }
